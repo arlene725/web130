ea467445f5075234cd1506cca15bc1504087fab8
Arlene Cazarez <arlene_c@mail.fresnostate.edu>
2016-01-31 13:40:26 -0800

    printing variable types

---
+++ varprint-type/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	typeInt := 20
+	typeBool := true
+	typeString := "Arlene"
+	typeFloat := 3.14
+
+	fmt.Printf("%T\n", typeInt)
+	fmt.Printf("%T\n", typeBool)
+	fmt.Printf("%T\n", typeString)
+	fmt.Printf("%T\n", typeFloat)
+
+}
