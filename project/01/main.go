e0a841ac999ec718303987cec882c2d8c6d0f1ad
Arlene Cazarez <arlene_c@mail.fresnostate.edu>
2016-03-12 14:52:52 -0800

    project step1

---
+++ project/01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import(
+	"net/http"
+	"html/template"
+)
+
+func main(){
+
+	tpl, err := template.ParseFiles("temp.html")
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request){
+		err = tpl.Execute(res, nil)
+	})
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
